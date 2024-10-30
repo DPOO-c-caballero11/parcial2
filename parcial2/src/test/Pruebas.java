9b4440fbbb9afb625d2c4677bf40f8a09b097f6a
camilacaballer <c.caballero11@uniandes.edu.co>
2024-10-30 09:19:46 -0500

    pequeno ajuste

--- parcial2/src/test/Pruebas.java
+++ parcial2/src/test/Pruebas.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTimeout;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 
 public class Pruebas {
